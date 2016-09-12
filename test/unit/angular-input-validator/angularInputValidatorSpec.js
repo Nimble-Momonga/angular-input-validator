e7d4cc1d5649b048fd72e573141ffcb8064db8c9
Nimble-Momonga <manuel_tejedor_77@hotmail.com>
2016-09-12 15:53:42 -0300

    fixed module tests

--- test/unit/angular-input-validator/angularInputValidatorSpec.js
+++ test/unit/angular-input-validator/angularInputValidatorSpec.js
@@ -2,41 +2,31 @@
 
 describe('', function() {
 
-  var module;
-  var dependencies;
-  dependencies = [];
+	var module;
+	var dependencies;
+	dependencies = [];
 
-  var hasModule = function(module) {
-  return dependencies.indexOf(module) >= 0;
-  };
+	var hasModule = function(module) {
+		return dependencies.indexOf(module) >= 0;
+	};
 
-  beforeEach(function() {
+	beforeEach(function() {
 
-  // Get module
-  module = angular.module('angularInputValidator');
-  dependencies = module.requires;
-  });
+		// Get module
+		module = angular.module('angularInputValidator');
+		dependencies = module.requires;
+	});
 
-  it('should load config module', function() {
-    expect(hasModule('angularInputValidator.config')).to.be.ok;
-  });
+	it('should load config module', function() {
+		expect(hasModule('angularInputValidator.config')).to.be.ok;
+	});
 
-  
-  it('should load filters module', function() {
-    expect(hasModule('angularInputValidator.filters')).to.be.ok;
-  });
-  
+	it('should load providers module', function() {
+		expect(hasModule('angularInputValidator.providers')).to.be.ok;
+	});
 
-  
-  it('should load directives module', function() {
-    expect(hasModule('angularInputValidator.directives')).to.be.ok;
-  });
-  
-
-  
-  it('should load services module', function() {
-    expect(hasModule('angularInputValidator.services')).to.be.ok;
-  });
-  
+	it('should load directives module', function() {
+		expect(hasModule('angularInputValidator.directives')).to.be.ok;
+	});
 
 });
